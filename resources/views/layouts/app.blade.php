55cfcdfe59c77dfa7191f50d80b74d6e1203170c
Kuur <dickynakiri@gmail.com>
2022-05-09 12:03:53 +0700

    First commit & Dashboard

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!-- Header -->
+@include('layouts.header')
+
+<!-- Navbar -->
+@include('layouts.navbar')
+
+<!-- Sidebar -->
+@include('layouts.sidebar')
+
+<!-- Content -->
+@yield('content')
+
+<!-- Footer -->
+@include('layouts.footer')
